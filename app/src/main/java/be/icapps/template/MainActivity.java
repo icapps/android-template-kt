b34636f73fa2a63d0c84fbede98305d3f24fc8d0
Marianne De Turck <marianne.deturck@icapps.com>
2017-04-03 10:51:12 +0200

    Added yml questionaire + 2 _OLLIE_ params

--- app/src/main/java/be/icapps/template/MainActivity.java
+++ app/src/main/java/be/icapps/template/MainActivity.java
@@ -3,6 +3,11 @@ package be.icapps.template;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+/*
+ * Created by _OLLIE_AUTHOR_
+ * Copyright © 2017 iCapps. All rights reserved.
+ */
+
 public class MainActivity extends AppCompatActivity {
 
 	@Override
